49036ffdc143e3d8d323974eea07865fe8749a60
rookie-leo <proleonarpa@gmail.com>
2022-03-31 09:13:43 -0300

    Meu primeiro component

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector:'meu-primeiro-component',
+    template:`
+        <p>Meu Primeiro Component com Angular 2</p>
+    `
+})
+export class MeuPrimeiroComponent {
+
+}
