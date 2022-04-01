78ebcc308043c8c2c8ee126413959c30448beda6
rookie-leo <proleonarpa@gmail.com>
2022-03-31 22:51:32 -0300

    Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
@@ -12,7 +14,9 @@ import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
     MeuPrimeiro2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
