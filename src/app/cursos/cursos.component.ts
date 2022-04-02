b544aa78a6a99866925c350e5f539663483bc1eb
rookie-leo <proleonarpa@gmail.com>
2022-04-01 21:09:34 -0300

    Introdução aos Serviços e Injeção de Dependencia

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
@@ -8,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class CursosComponent implements OnInit {
 
   nomePortal: string;
-  cursos: string[] = ['Java', 'Ext JS', 'Angular'];
-  constructor() {
+  cursos: string[] = [];
+
+  constructor(private cursoService: CursosService) {
     this.nomePortal = 'https://loiane.training/';
+    this.cursos = this.cursoService.getCursos();
   }
+
+
 
   ngOnInit(): void {
   }
